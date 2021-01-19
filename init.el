bd59afab6d0a83cb496e8b65a2224fdb70699ae8
Allen <allen.j.blount@gmail.com>
2021-01-19 16:05:42 -0500

    added org mode lines

--- init.el
+++ init.el
@@ -68,4 +68,9 @@
 
 (add-hook 'before-save-hook  'force-backup-of-buffer)
 
-
+;; org-mode
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+(setq org-agenda-files '("~/org"))
